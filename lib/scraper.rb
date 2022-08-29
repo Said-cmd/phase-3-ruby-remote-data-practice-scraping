cb0441c6997398b7e5300f9edffd480c76ca9fe4
Said Hussein <saidhussein12342@gmail.com>
2022-08-29 11:42:13 +0300

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+buttons = doc.css(".btn.btn-primary.btn-red")
+
+buttons.each do |button|
+    puts button.text.strip
+end
+
